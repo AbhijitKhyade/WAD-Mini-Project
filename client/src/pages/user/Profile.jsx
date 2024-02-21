72e6acab297679678a0985f6f350ae68ad1da49e
AbhijitKhyade <abhijitkhyade@gmail.com>
2024-02-21 19:41:01 +0530

    first commit

---
+++ client/src/pages/user/Profile.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+
+import Layout from "./../../components/Layout/Layout";
+// eslint-disable-next-line no-unused-vars
+import UserMenu from "./../../components/Layout/UserMenu";
+import { useAuth } from "../../context/auth";
+import "../../styles/AuthStyles.css";
+import { BASE_URL } from './../../api';
+
+const Profile = () => {
+  const [auth, setAuth] = useAuth();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  const navigate = useNavigate();
+
+  //get user data
+  useEffect(() => {
+    const { email, name, phone, address } = auth?.user;
+    setName(name);
+    setPhone(phone);
+    setEmail(email);
+    setAddress(address);
+  }, [auth?.user]);
+
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.put(`${BASE_URL}/auth/profile`, {
+        name,
+        email,
+        phone,
+        address,
+      });
+      if (data?.error) {
+        toast.error(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data?.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        // toast.success("Profile Updated Successfully");
+        toast.success(data?.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        navigate("/dashboard/user/orders");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message, {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+  return (
+    <Layout title={"Profile - Ecommerce App"}>
+      <div className="form-container ">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">Profile</h4>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              placeholder="Enter Your Name"
+              required
+              autoFocus
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              placeholder="Enter Your Email "
+              required
+              disabled
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              placeholder="Enter Your Phone"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              placeholder="Enter Your Address"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Update
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Profile;
